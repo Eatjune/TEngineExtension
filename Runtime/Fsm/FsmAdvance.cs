c612891b812bd9c440a9fb56b6856c58ee1d7bd6
Eatjune <490359426@qq.com>
2026-01-28 13:51:12 +0800

    增加状态机和本地化工具

---
+++ Runtime/Fsm/FsmAdvance.cs
@@ -0,0 +1,47 @@
+﻿using TEngine;
+
+namespace GameLogic {
+	public class FsmStateAdvance<T> : FsmState<T> where T : class {
+		protected T m_owner;
+		protected IFsm<T> m_fsm;
+
+		public void Init(IFsm<T> fsm, T owner) {
+			m_fsm = fsm;
+			m_owner = owner;
+			OnInit();
+		}
+
+		/// <summary>
+		/// 延迟初始化(所有状态已经创建完毕）
+		/// </summary>
+		protected virtual void OnInit() {
+		}
+
+		/// <summary>
+		/// 切换当前有限状态机状态。
+		/// </summary>
+		/// <typeparam name="TState">要切换到的有限状态机状态类型。</typeparam>
+		protected new void ChangeState<TState>() where TState : FsmState<T> {
+			if (m_fsm.CurrentState is TState) return;
+			base.ChangeState<TState>(m_fsm);
+		}
+	}
+
+	public static class FsmExtensions {
+		/// <summary>
+		/// 创建高级有限状态机
+		/// </summary>
+		/// <typeparam name="T">有限状态机持有者类型。</typeparam>
+		/// <param name="owner">有限状态机持有者。</param>
+		/// <param name="states">有限状态机状态集合。</param>
+		/// <returns>要创建的有限状态机。</returns>
+		public static IFsm<T> CreateFsmAdvanced<T>(this IFsmModule fsmModule, T owner, params FsmStateAdvance<T>[] states) where T : class {
+			var fsm = fsmModule.CreateFsm(string.Empty, owner, states);
+			foreach (var fsmStateAdvance in states) {
+				fsmStateAdvance.Init(fsm, owner);
+			}
+
+			return fsm;
+		}
+	}
+}
---
+++ Runtime/Localization/LocalizationUtils.cs
@@ -0,0 +1,27 @@
+﻿namespace GameLogic {
+	public static class LocalizationUtils {
+		/// <summary>
+		/// 获取本地化值
+		/// </summary>
+		public static string GetString(string key) {
+			var content = TEngine.Localization.LocalizationManager.GetTranslation(key);
+			if (string.IsNullOrEmpty(content)) {
+				content = key;
+			}
+
+			return content;
+		}
+
+		/// <summary>
+		/// 是否有该key
+		/// </summary>
+		public static bool HasString(string key) {
+			var content = TEngine.Localization.LocalizationManager.GetTranslation(key);
+			if (string.IsNullOrEmpty(content)) {
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
